ef337d74088198620fa57b5677b9ca0c4cf5b7e6
qatrainingjaguila <jaguila@academytrainee.com>
2020-12-02 12:53:21 +0000

    with STYLE

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -3,6 +3,7 @@ const Homepage = () => {
 return( 
     <>
     <h1>Welcome to Carlos' Movie Database!</h1>
+    <img src="https://www.hitc.com/static/uploads/2020/10/Carl-Up.jpg" style = {{width:"100%"}}></img>
     </>
  );
 }
